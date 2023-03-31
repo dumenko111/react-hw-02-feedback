46b6fcb9b3422181b769f6b69bc51552333372a6
Andriy Dumenko <101402719+dumenko111@users.noreply.github.com>
2023-03-31 11:00:02 +0300

    initial commit

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,32 @@
-export const App = () => {
-  return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
-  );
-};
+import { Component } from 'react';
+
+class App extends Component {
+  state = {
+    good: 0,
+    neutral: 0,
+    bad: 0,
+  };
+
+  checkingStatistics = e => {
+    console.log(e.target.value);
+  };
+
+  render() {
+    return (
+      <div>
+        <h1>Please leave feedback</h1>
+
+        <button onClick={this.checkingStatistics}>Good</button>
+        <button>Nutrale</button>
+        <button>Bad</button>
+
+        <h2>Statistics</h2>
+        <p>Good: {this.state.good}</p>
+        <p>Neutral: {this.state.neutral}</p>
+        <p>Bad: {this.state.bad}</p>
+      </div>
+    );
+  }
+}
+
+export default App;
